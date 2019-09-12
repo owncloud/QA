020e33b12ee86926a3e4c5cddcf11a7695562a55
Jürgen Weigert <jw@owncloud.com>
2019-09-12 20:56:37 +0200

    v0.5: option -t added, and several other safeguards.

--- Desktop/randfiles/randfiles.py
+++ Desktop/randfiles/randfiles.py
@@ -16,16 +16,28 @@
 #   Filesystem features:  has_journal ext_attr resize_inode dir_index filetype needs_recovery extent 64bit flex_bg sparse_super large_file huge_file dir_nlink extra_isize metadata_csum
 #  We can add large_dir (in addition to dir_index) option like this:
 #   sudo tune2fs -O large_dir /dev/sda2
+#
+# CAUTION: Windows has short 260 char limits, even when using NTFS.
+#  * In the latest versions of Windows, this limitation can be expanded to
+#    approximately 32,000 characters. Your administrator will need to activate
+#    the "Enable Win32 long paths" group policy, or set the registry value
+#    HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem@LongPathsEnabled to 1.
+#  * https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#maximum-path-length-limitation
+#  * https://docs.python.org/3/using/windows.html
+#
+# TODO: On Linux, even deeper paths may be possible when using chdir() into each subdirectory.
 
 # v0.1 -- 2019-09-03, jw        initial draught
 # v0.2 -- 2019-09-05, jw        adaptive min_entries_per_dir and folder_ratio for very tall trees.
 # v0.3 -- 2019-09-09, jw        target dir mandatory, option parser added.
+# v0.4 -- 2019-09-11, jw        lower default folder ratio from 2.0% to 0.5%, fixed the logic for low -e values.
+# v0.5 -- 2019-09-12, jw        new -t option to test max tree dimensions.
 
 
 import random, time, string, os, sys, json
-import argparse
+import argparse, shutil
 
-__version__ = '0.3'
+__version__ = '0.5'
 
 conf = {
   'maxfiles': 1_000_000,
@@ -36,7 +48,7 @@ conf = {
   'max_name_len': 200,
   'min_body_len': 1,
   'max_body_len': 1_000,
-  'folder_ratio': 2.0,                # 2.0/100=0.02: 98% of all objects, are files; 2% are folders.
+  'folder_ratio': 0.5,                # 2.0/100=0.02: 98% of all objects, are files; 2% are folders.
   'corpus_size': 2_000_000,
 }
 
@@ -59,6 +71,7 @@ parser.add_argument('-b', '--max_body_len', metavar='MAX_BODY_LEN', type=int, he
 parser.add_argument(      '--min_body_len', metavar='MIN_BODY_LEN', type=int, help='Minimum length file contents. Default: ' + str(conf['min_body_len']))
 parser.add_argument('-c', '--corpus_size', metavar='CORPUS_SIZE', type=int, help='Size of the internal random pool. (Larger is more chaotic but slower). Default: ' + str(conf['corpus_size']))
 parser.add_argument('-L', '--load_config', metavar='CONFIG_FILE', type=str, help='Load a config file from a previous run, before applying other options. Default: none.')
+parser.add_argument('-t', '--testonly', action='store_true', help='Only test how deep or wide a tree could be. Default: create a tree.')
 args = parser.parse_args()
 
 if args.load_config:
@@ -83,14 +96,18 @@ if conf['max_entries_per_dir'] < 5000:
     conf['min_entries_per_dir'] = maxmin
 
 if conf['max_entries_per_dir'] < 500:
-  if conf['folder_ratio'] > 0.02:
+  if conf['folder_ratio'] < 1.0:
     # try avoid running out of subdirs
-    print("Reducing folder_ratio from %g to %g due to low max_entries_per_dir." % (conf['folder_ratio'], 0.02))
-    conf['folder_ratio'] = 0.02
+    print("Increasing folder_ratio from %g to %g due to low max_entries_per_dir." % (conf['folder_ratio'], 1.0))
+    conf['folder_ratio'] = 1.0
   if conf['max_name_len'] > 50:
     # try avoid crashing against MAXPATH trivially.
     print("Reducing max_name_len from %d to %d due to low max_entries_per_dir." % (conf['max_name_len'], 50))
     conf['max_name_len'] = 50
+
+if conf['min_name_len'] > conf['max_name_len']:
+  print("Reducing min_name_len from %d to %d due to low max_name_len." % ( conf['min_name_len'], max(1, int(conf['max_name_len']/10)) ))
+  conf['min_name_len'] = max(1, int(conf['max_name_len']/10))
 
 if conf['corpus_size'] < conf['max_name_len']:
   print("ERROR: corpus_size=%d must be at least max_name_len=%d." % (conf['corpus_size'], conf['max_name_len']))
@@ -101,6 +118,104 @@ if conf['corpus_size'] < conf['max_body_len']:
   sys.exit(1)
 
 conffile = os.path.basename(os.path.abspath(args.dir))+'.conf'
+
+os.makedirs(args.dir, exist_ok=True)
+os.chdir(args.dir)      # or explode.
+
+if args.testonly:
+  incr = 10
+  testdepth = 10_000
+  testwidth = 20_000
+  testnamelen = 10_000
+  testfolder = "_t"
+  if sys.platform == 'win32':
+    testdepth = 1_000           # Windows 10 on NTFS with LongPathsEnabled=1, 10_000 takes ages.
+    testwidth = 2_000           # Windows 10 on NTFS is just dead slow.
+
+  def myrmtree(path):
+    sys.setrecursionlimit(testdepth+2)
+
+    try:        # linux only: avoid soft limits.
+      import resource
+      a = resource.getrlimit(resource.RLIMIT_NOFILE)
+      resource.setrlimit(resource.RLIMIT_NOFILE, (a[1], a[1]))
+    except:
+      pass
+
+    try:
+      shutil.rmtree(path)
+    except OSError as e:
+      print("myrmtree: had an OSError. errno=%d strerror=%s" % (e.errno, e.strerror))
+      if e.errno == 24:         # Too many open files
+        print("Try switching to superuser, run\n  ulimit -n %d\n\n... or higher, then retry." % (testdepth+2))
+        sys.exit(1)
+      else:
+        raise(e)
+
+  os.makedirs(testfolder, exist_ok=False)       # explode early, if another test is running here.
+  for l in range(incr, testnamelen, incr):
+    path = testfolder +"/" + "f" * l   # one file with long name
+    try:
+      fd = open(path, "w")
+    except:
+      print("open() with a long file name:          limit exceeded at namelen=%d (...+%d), pathlen=%d\n" % (l-incr+1, incr-1, len(path)))
+      break
+    else:
+      fd.close()
+      os.unlink(path)
+  else:
+    print("open() with a long file name:            (testnamelen=%d done; pathlen=%d)" % (testnamelen, len(path)))
+  myrmtree(testfolder)
+
+  for l in range(incr, testnamelen, incr):
+    path = testfolder + "/" + "f" * l   # one folder with long name
+    try:
+      os.makedirs(path)
+    except:
+      print("makedirs() with one long folder name:  limit exceeded at namelen=%d (...+%d), pathlen=%d" % (l-incr+1, incr-1, len(path)))
+      break
+    else:
+      os.rmdir(path)
+  else:
+    print(  "makedirs() with one long folder name:  (testnamelen=%d done; pathlen=%d)" % (testnamelen, len(path)))
+  myrmtree(testfolder)
+
+  for d in range(testdepth):
+    fname = "123456789_123456789_123456789_123456789_123456789"
+    path = testfolder + ("/" + fname) * d
+    try:
+      os.makedirs(path)
+    except:
+      print("makedirs() with long folder names:     limit exceeded at depth=%d, pathlen=%d (...+%d)" % (d-1, len(path)-len(fname), len(fname)))
+      break
+  else:
+    print(  "makedirs() with long folder names:     (testdepth=%d done; pathlen=%d)" % (testdepth, len(path)))
+  myrmtree(testfolder)
+
+  for d in range(incr, testdepth, incr):
+    path = testfolder + "/f" * d
+    try:
+      os.makedirs(path)
+    except:
+      print("makedirs() with short folder names:    limit exceeded at depth=%d (...+%d), pathlen=%d" % (d-incr+1, incr-1, len(path)))
+      break
+  else:
+    print(  "makedirs() with short folder names:    (maxdepth=%d done; pathlen=%d)" % (testdepth, len(path)))
+  myrmtree(testfolder)
+
+  for d in range(testwidth):
+    path = testfolder + ("/f_%08d" % d)
+    try:
+      os.makedirs(path)
+    except:
+      print("makedirs() with many sibling folders:  limit exceeded with number_of_siblings=%d" % (d))
+      break
+  else:
+    print(  "makedirs() with many sibling folders:  (testwidth=%d done)" % (testwidth))
+  myrmtree(testfolder)
+
+  sys.exit(0)
+
 
 random.seed(conf['seed'])
 corpus = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(conf['corpus_size']))
@@ -123,9 +238,6 @@ def randfolder():
   name = randstr(conf['min_name_len'], conf['max_name_len'])
   return name
 
-
-os.makedirs(args.dir, exist_ok=True)
-os.chdir(args.dir)      # or explode.
 
 dirs = [['.']]
 
