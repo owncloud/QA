8d3f56ff03cd3e660057643297935bec18cc01b4
Juergen Weigert <jnweiger@gmail.com>
2021-05-28 00:59:26 +0200

    update to allow multiple ssh_keys...

--- tools/ansible_playground/hetzner_deployments_terraform_ansible/owncloud_demo_single_server/hcloud.tf
+++ tools/ansible_playground/hetzner_deployments_terraform_ansible/owncloud_demo_single_server/hcloud.tf
@@ -10,30 +10,34 @@ terraform {
 variable "hcloud_token" {}
 variable "owner_tag" {}
 variable "labels" {}
+variable "key_names" {}
+variable "machine_name" {}
+variable "image" {}
+variable "server_type" {}
 
 provider "hcloud" {
-  token = "${var.hcloud_token}"
+  token = var.hcloud_token
 }
 
-data "hcloud_ssh_key" "mykey" {
-  name = "gleikam"
+data "hcloud_ssh_key" "sshkeys" {
+  count = length(var.key_names)
+  name = var.key_names[count.index]
 }
-
 
 resource "hcloud_server" "owncloud_server" {
-  name        = "${var.owner_tag}-terraform-owncloud-test-server"
-  image       = "ubuntu-20.04"
-  server_type = "cx11"
-  ssh_keys = [ "${data.hcloud_ssh_key.mykey.id}" ]
-  labels = "${var.labels}"
+  name        = "${var.owner_tag}-${var.machine_name}"
+  image       = var.image
+  server_type = var.server_type
+  ssh_keys    = data.hcloud_ssh_key.sshkeys[*].id
+  labels      = var.labels
 
   provisioner "remote-exec" {
     inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
 
     connection {
-      host        = self.ipv4_address
-      type        = "ssh"
-      user        = "root"
+      host = self.ipv4_address
+      type = "ssh"
+      user = "root"
     }
   }
   provisioner "local-exec" {
@@ -51,5 +55,9 @@ output "owncloud_server_ip" {
   depends_on = [
     hcloud_server.owncloud_server
   ]
+}
+
+output "sshkeyoutput" {
+  value = data.hcloud_ssh_key.sshkeys[*].name
 }
 
--- tools/ansible_playground/hetzner_deployments_terraform_ansible/owncloud_demo_single_server/terraform.tfvars
+++ tools/ansible_playground/hetzner_deployments_terraform_ansible/owncloud_demo_single_server/terraform.tfvars
@@ -1,9 +1,18 @@
 hcloud_token = "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+
+machine_name = "terraform-owncloud-test-server"
+image = "ubuntu-20.04"
+server_type = "cx11"
+
+#owner tag. used as praefix for machine name
+owner_tag = "gl"
+
+#ssh key names
+key_names = ["gleikam", "jw@owncloud.com"]
+
+#machine labels
 labels = {
   owner:"gleikam",
   usedfor:"terraformtesting",
   madewith:"terraform"
 }
-owner_tag = "gl"
-
-
