045a5babc786e6308408f39c1495b34e273a6a1c
Juergen Weigert <jnweiger@gmail.com>
2021-09-10 12:16:47 +0200

    update to 10.8.0 --- but still does not work :-(

--- tools/hetzner-deploy/setup_behat.sh
+++ tools/hetzner-deploy/setup_behat.sh
@@ -7,9 +7,9 @@
 
 echo "Estimated setup time: 8 minutes ..."
 
-vers=10.6.0beta1
+vers=10.8.0
 d_vers=$(echo $vers  | tr '[A-Z]' '[a-z]' | tr . -)	# -$(date +%Y%m%d)
-source lib/make_machine.sh -u acceptance-$d_vers -p git,screen,docker.io,composer,make,libxml2-utils,apache2,libapache2-mod-php,openssl,php-imagick,php-common,php-curl,php-gd,php-imap,php-intl,php-json,php-mbstring,php-mysql,php-ssh2,php-xml,php-zip,php-apcu,php-ldap,php-gmp,wget "$@"
+source lib/make_machine.sh -u acceptance-$d_vers -p git,screen,docker.io,composer,make,libxml2-utils,apache2,libapache2-mod-php,openssl,php-imagick,php-common,php-curl,php-gd,php-imap,php-intl,php-json,php-mbstring,php-mysql,php-ssh2,php-xml,php-zip,php-apcu,php-ldap,php-gmp,wget,npm "$@"
 
 # from https://github.com/owncloud-docker/server/pull/204#issuecomment-714388044
 example_make_cmd="make test-acceptance-webui BEHAT_FEATURE=tests/acceptance/features/webUIAdminSettings/adminAppsSettings.feature"
@@ -29,6 +29,11 @@ EOCC
   cd owncloud
   git clone https://github.com/owncloud/testing apps/testing
 
+  npm install -g yarn
+  make clean
+  make install-composer-deps
+  make update-composer
+  make install-nodejs-deps
   $example_make_cmd
 
   cat <<EOM
