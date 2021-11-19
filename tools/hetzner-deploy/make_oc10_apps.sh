4d7518f56cb418d1bf072ab4bdf6c93e59f3417d
Juergen Weigert <jnweiger@gmail.com>
2021-11-19 23:05:16 +0100

    defauult to 10.9 beta1

--- tools/hetzner-deploy/make_oc10_apps.sh
+++ tools/hetzner-deploy/make_oc10_apps.sh
@@ -18,7 +18,8 @@
 echo "Estimated setup time: 5 minutes ..."
 
 vers=10.8.0
-tar=https://download.owncloud.org/community/owncloud-complete-20210721.tar.bz2
+vers=10.9.0beta1
+tar=https://download.owncloud.org/community/testing/owncloud-complete-20211118.tar.bz2
 test -n "$OC_VERSION" && vers="$OC_VERSION"
 test -n "$OC10_VERSION" && vers="$OC10_VERSION"
 test "$vers" = "10.8.0"       -o "$vers" = "10.8"        && tar=https://download.owncloud.org/community/owncloud-complete-20210721.tar.bz2
