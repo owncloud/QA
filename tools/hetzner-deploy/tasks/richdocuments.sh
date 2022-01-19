77e03b9b09d5769099a03f1eaabe643aa40ab100
Juergen Weigert <jnweiger@gmail.com>
2022-01-19 13:17:58 +0100

    do not enable secure-view by default, that spoils the testplan.

--- tools/hetzner-deploy/tasks/richdocuments.sh
+++ tools/hetzner-deploy/tasks/richdocuments.sh
@@ -1,15 +1,14 @@
-occ app:enable richdocuments	# CAUTION: triggers license grace period!
+occ app:enable richdocuments	# Does not trigger license grace period, but secureview is unavailable in the UI without license
+occ app:enable pdf_viewer	# for testing interoperability
 
 ## configure one of our OLD trusty servers. TODO: deploy own collabora host.
 # occ config:app:set richdocuments wopi_url --value https://collabora.owncloud.works:443
 occ config:app:set richdocuments wopi_url --value https://collabora.owncloud-demo.com:443
 
-## enable secure view. TODO: can this be controlled per file or folder?
-occ config:app:set richdocuments secure_view_option                --value true	# true requires an enterprise license
+## enable secure view. The occ command does not check the license. -> https://github.com/owncloud/richdocuments/issues/442
 occ config:app:set richdocuments secure_view_has_watermark_default --value true
 occ config:app:set richdocuments secure_view_can_print_default     --value false
 occ config:app:set richdocuments secure_view_open_action_default   --value true	# TODO: what does this do?
-occ app:enable pdf_viewer
 
 ## TODO: create a folder share between admin and user1 in secure view mode.
 # https://doc.owncloud.com/server/admin_manual/enterprise/collaboration/collabora_secure_view.html#how-to-enable-secure-view
@@ -18,7 +17,10 @@ occ app:enable pdf_viewer
 # - jpg and other non-securable files are not visible at all.
 cat << EOM >>  ~/POSTINIT.msg
 richdocuments:  Also enabled app pdf_viewer
-richdocuments:  secure_view: open_action_default=true, has_watermark_default=true, can_print_default=false
+richdocuments:  Secure_view settings: open_action_default=true, has_watermark_default=true, can_print_default=false
+richdocuments:	Do this manually after checking that it is initially grayed out:
+richdocuments:		occ config:app:set richdocuments secure_view_option --value true
+richdocuments:
 richdocuments:  Please add an enterprise license and check the secure view settings in admin->additional
 richdocuments:  Please create a share with a user (or group) and check '[x] Secure View' in the sharing permissions
 --------------------------------------------------------
