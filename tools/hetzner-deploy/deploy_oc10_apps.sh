8feb4403248625d5df396cc9d0bc5a52e268ed5c
Juergen Weigert <jnweiger@gmail.com>
2022-05-09 21:40:23 +0200

    RC2 testing now.

--- tools/hetzner-deploy/deploy_oc10_apps.sh
+++ tools/hetzner-deploy/deploy_oc10_apps.sh
@@ -18,10 +18,11 @@
 
 echo "Estimated setup time: 5 minutes ..."
 
-vers=10.10.0RC1
+vers=10.10.0RC2
 
 test -n "$OC_VERSION" && vers="$OC_VERSION"
 test -n "$OC10_VERSION" && vers="$OC10_VERSION"
+test "$vers" = "10.10.0RC2"                     && tar=https://download.owncloud.com/server/testing/owncloud-complete-20220509.tar.bz2
 test "$vers" = "10.10.0RC1"                     && tar=https://download.owncloud.com/server/testing/owncloud-complete-20220504.tar.bz2
 test "$vers" = "10.9.1RC2"                      && tar=https://download.owncloud.com/server/testing/owncloud-complete-20220111.tar.bz2
 test "$vers" = "10.9.1RC1"                      && tar=https://download.owncloud.com/server/testing/owncloud-complete-20211228.tar.bz2
