8a0bf840f195d3c915333ebc1808f5d89ca110fa
Juergen Weigert <jnweiger@gmail.com>
2022-12-02 16:57:04 +0100

    admin console has its own --hostname thing... grrr.

--- tools/hetzner-deploy/deploy_keycloak.sh
+++ tools/hetzner-deploy/deploy_keycloak.sh
@@ -8,10 +8,13 @@
 #
 # - https://www.keycloak.org/server/containers
 # - https://www.keycloak.org/server/configuration-production
+# - https://www.keycloak.org/server/hostname
 
 
 test -z "$HCLOUD_MACHINE_TYPE" && HCLOUD_MACHINE_TYPE=cx11
 machine_type=$HCLOUD_MACHINE_TYPE
+
+HTTPS_PORT=19443
 test -z "$KEYCLOAK_DNSNAME" && KEYCLOAK_DNSNAME="keycloak-DATE.jw-qa.owncloud.works"
 KEYCLOAK_DNSNAME=$(echo $KEYCLOAK_DNSNAME | sed -e "s/DATE/$(date +%Y%m%d)/")
 d_name=$(echo $KEYCLOAK_DNSNAME  | sed -e "s/\..*//" | tr '[A-Z]' '[a-z]' | tr . -)
@@ -31,6 +34,7 @@ echo >> ~/env.sh "oc10_fqdn=$KEYCLOAK_DNSNAME"	# queried by make_machine.sh from
 
 # Health check endpoints are available at https://localhost:8443/health,
 # https://localhost:8443/health/ready and https://localhost:8443/health/live.
+# - /health/ready is the same as /health, /health/live is a bit shorter, its 'checks' list is empty.
 # Use with --health-enabled
 #
 # The published Keycloak containers have a directory /opt/keycloak/data/import.
@@ -40,14 +44,15 @@ echo >> ~/env.sh "oc10_fqdn=$KEYCLOAK_DNSNAME"	# queried by make_machine.sh from
 # Dev mode.
 #
 # If enabled, metrics are available at the '/metrics' endpoint.
+# -> it contains many details about cpu and memory usage, but nohting about realms or users.
 #
 # keycloak's docker container advertises port 8443, but there is nothing per default in development mode.
-# It has a hostname and a portnumber option.
-# --hostname=localhost --hostname-port=17443
+# It has a hostname and a portnumber option, but that does not enable ssl.
+# --hostname=https://www.keycloak.org/server/hostname --hostname-port=19443
 #
 screen -d -m -S keycloak -Logfile screenlog-keycloak -L \
-  docker run --rm --name keycloak -ti -e KEYCLOAK_ADMIN_PASSWORD=kadmin -e KEYCLOAK_ADMIN=kadmin -p 17443:8443 -p 4488:8080 quay.io/keycloak/keycloak \
-    start-dev --import-realm --metrics-enabled=true --health-enabled=true --hostname-port=17443
+  docker run --rm --name keycloak -ti -e KEYCLOAK_ADMIN_PASSWORD=kadmin -e KEYCLOAK_ADMIN=kadmin -p 4488:8080 quay.io/keycloak/keycloak \
+    start-dev --import-realm --metrics-enabled=false --health-enabled=true --hostname=$KEYCLOAK_DNSNAME --hostname-port=$HTTPS_PORT --hostname-admin-url=https://$KEYCLOAK_DNSNAME:$HTTPS_PORT/
 
 # wait until keycloak is up:
 for i in 10 9 8 7 6 5 4 3 2 1 last; do
@@ -65,7 +70,6 @@ keycloak_ip=\$(docker inspect keycloak | jq '.[0].NetworkSettings.IPAddress' -r)
 BACKEND_HTTP_SERVER="http://\$keycloak_ip:8080"
 SSL_CERT_FILE=./fullcert.pem		# must contain a /
 SSL_KEY_FILE=./privkey.pem
-HTTPS_PORT=19443
 
 
 
@@ -105,9 +109,25 @@ done
 certfile=\$(echo "\$privkey" | sed -e 's/privkey\.pem/fullchain.pem/')
 cp \$certfile \$privkey proxy/
 
+
 # CAUTION: keep in sync with bin/nginx_ssl_proxy
+#   netwok_mode: bridge
+# is supposed to be a shortcut for
+#
+#   network:
+#     - host
+# networks:
+#   host:
+#     name: bridge
+#     external: true
+#
+# but that failes with one of:
+#  networks.host value Additional properties are not allowed ('name' was unexpected)
+#  network-scoped alias is supported only for containers in user defined networks
+#
+
 cat << DC_YML > proxy/docker-compose.yml
-version: "3"
+version: "3.0"
 services:
   nginx_ssl_proxy:
     image: "nginx:latest"
