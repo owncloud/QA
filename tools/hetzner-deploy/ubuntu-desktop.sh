eaeca35ed180ba1ac837042cf058ed5c2c63597d
Juergen Weigert <jnweiger@gmail.com>
2023-02-22 19:47:43 +0100

    scripted startup of a full bloated ubuntu mate desktop

---
+++ tools/hetzner-deploy/ubuntu-desktop.sh
@@ -0,0 +1,25 @@
+export DEBIAN_FRONTEND=noninteractive	# try prevent ssh install to block wit whiptail
+export LC_ALL=C LANGUAGE=C
+aptQ() { echo "+ apt $@"; apt "$@" 2>&1 | stdbuf -o0 tr ] '\n' | grep -E -v "^(Preparing to|Get:|Selecting previously unselected|^WARNING: apt does not have|Creating config|Created symlink|Processing triggers|^Fetched |^Setting up |^Need to |^After this |^\s*\(Reading |^Reading |^Building|^\$|^0 upgraded, )"; test ${PIPESTATUS[0]} -eq 0; }
+
+aptQ update
+aptQ upgrade -y
+aptQ install -y software-properties-common
+aptQ install -y x11vnc ubuntu-mate-desktop	# ca 2GB of packages
+apt purge mate-power-manager	# it crashes on a VM
+systemctl set-default graphical
+
+# recommended by /home/testy/HOWTO/remote-desktop-sharing.txt
+aptQ install -y x2goserver x2goserver-xsession
+testypass="$(tr -dc 'a-z0-9' < /dev/urandom | head -c 10)"
+echo -e "$testypass\n$testypass" | adduser testy --gecos ""
+mkdir ~testy/.ssh
+cp .ssh/authorized_keys ~testy/.ssh/
+chown -R testy. ~testy/.ssh
+chmod 700 /home/testy/.ssh
+
+echo "Now reboot... and connect with x2goclient"
+echo "	Host: $IPADDR"
+echo "	User: testy"
+echo "	Password: $testypass"
+
