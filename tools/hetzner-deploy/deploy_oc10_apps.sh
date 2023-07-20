0956a2c00370b47acfc7e34fabe1bcd6269e4a13
Juergen Weigert <jnweiger@gmail.com>
2023-07-20 14:24:55 +0200

    OC10_DNSNAME sanitazion improved...

--- tools/hetzner-deploy/deploy_oc10_apps.sh
+++ tools/hetzner-deploy/deploy_oc10_apps.sh
@@ -225,10 +225,11 @@ test "$firstarg" = "-" && firstarg=
 # try to keep the name short. certbot explodes on long names
 firstarg=$(echo "$firstarg" | sed -e 's/^\-windows_network_drive/-wnd/' -e 's/^\-ransomware_protection/-rwp/' -e 's/^\-user_/-/' -e 's/^\-files_/-/')
 # make dns/docker friendly: lowercase all; remove .; _ to -; shorten -alpha, -beta to a,b in both oc10 and app version.
-test -z "$OC10_DNSNAME" && OC10_DNSNAME="$(echo "oc$vers$firstarg" | tr '[A-Z]_' '[a-z]-' | tr -d .=+ | sed -e 's/-\?alpha/a/g' -e 's/-\?beta/b/g' -e 's/-\?rc/rc/g')-DATE"
+test -z "$OC10_DNSNAME" && OC10_DNSNAME="$(echo "oc$vers$firstarg" | tr -d .=+ | sed -e 's/-\?alpha/a/g' -e 's/-\?beta/b/g' -e 's/-\?rc/rc/g')-DATE"
+OC10_DNSNAME="$(echo "$OC10_DNSNAME" | tr '[A-Z]_' '[a-z]-' | tr -d =+ | tr ._ -)"
 h_name="$OC10_DNSNAME"
 test -z "$h_name" && h_name=oc-$vers-DATE
-d_name=$(echo $h_name  | sed -e "s/DATE/$(date +%Y%m%d)/" | tr '[A-Z]' '[a-z]' | tr . -)
+d_name=$(echo $h_name  | sed -e "s/DATE/$(date +%Y%m%d)/")
 
 if [ -z "$HCLOUD_MACHINE_TYPE" ]; then
   machine_type=cx11
