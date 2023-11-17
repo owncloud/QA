d46a863c92d6ef4632283500cb964daed52ad2a8
Juergen Weigert <jnweiger@gmail.com>
2023-11-17 13:30:29 +0100

    local collabora works with HTTP

--- tools/hetzner-deploy/tasks/richdocuments.sh
+++ tools/hetzner-deploy/tasks/richdocuments.sh
@@ -4,10 +4,17 @@
 #
 source ./env.sh		# needed for own IP addr with collabora server
 
+collabora_docker=collabora/code:latest
 # More possible extra params: --o:ssl.termination=true --o:welcome.enable=false --o:net.frame_ancestors=${OCIS_DOMAIN:-ocis.owncloud.test}
-docker run -t -d -p 9980:9980 -e "extra_params=--o:ssl.enable=false" collabora/code
-echo sleep 2 ...
-sleep 2
+docker run -t -d -p 9980:9980 -e "extra_params=--o:ssl.enable=false" $collabora_docker
+while true; do
+  echo sleep 2 ...
+  sleep 2
+  cool="$(curl -sI http://$IPADDR:9980 | sed -n -e 's@^\(HTTP/[0-9.]*\|Server:\)\s*@@p' | tr '\n\r' ' ')"
+  echo "$collabora_docker responds with: \"$cool\""
+  # loop until we see a 200 OK, and then print the server name, which includes a version.
+  echo $cool | grep '200 OK' 2>&1 > /dev/null && break
+done
 
 occ app:enable richdocuments	# Does not trigger license grace period, but secureview is unavailable in the UI without license
 # occ app:enable pdf_viewer	# for testing interoperability
@@ -36,6 +43,10 @@ richdocuments:		occ config:app:set richdocuments secure_view_option --value true
 richdocuments:
 richdocuments:  Please add an enterprise license and check the secure view settings in admin->additional
 richdocuments:  Please create a share with a user (or group) and check '[x] Secure View' in the sharing permissions
+richdocuments:
+richdocuments:  ATTENTION: the local docker server $collabora_docker is HTTP only. To not get blocked in browser
+richdocuments:		   redirects, you have to connect to owncloud with HTTP too:
+richdocuments:		http://$IPADDR:80
 richdocuments:
 richdocuments:  Try the old collabora.owncloud-demo.com server:
 richdocuments:  	occ config:app:set richdocuments wopi_url --value https://collabora.owncloud-demo.com:443
