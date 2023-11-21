a2f99f89f4977a6829633c3aa34f2d74203d9092
Juergen Weigert <jnweiger@gmail.com>
2023-11-21 18:56:55 +0100

    unfinished draft of portal_upload_server.sh ending with printend instructions...

--- tools/portal_upload_server.sh
+++ tools/portal_upload_server.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 dl=https://download.owncloud.com/server/stable/
-suf="tar.bz2 tar.bz2.asc tar.bz2.sha256 tar.bz2.md5 zip zip.asc zip.sha256 zip.md5"
+suf="tar.bz2.asc tar.bz2.sha256 tar.bz2.md5 tar.bz2 zip.asc zip.sha256 zip.md5 zip"
 
 if [ -z "$1" ]; then
 cat <<EOF
@@ -17,6 +17,7 @@ The first argument will be used to also find a
 third-party-licenses-....zip file
   
 EOF
+  exit 1
 fi
 
 
@@ -30,4 +31,17 @@ for v in $*; do
 done
 wget -nv $dl/third-party-licenses-$1.zip
 
-echo $tmpdir
+cat <<EOF
+Files downloaded to $tmpdir
+
+Do this
+ 1) Login as ldap user
+ 2) Create a new subfolder for the ownCloud version e.g. oC-10.13.3
+ 3) Upload all 17 files.
+
+for each of the following URLs:
+* https://customer.owncloud.com/owncloud/index.php/apps/files/?dir=%2FownCloud-Server%2FownCloud-10.0
+* https://customer.owncloud.com/owncloud/index.php/apps/files/?dir=%2FownCloud-Enterprise%2FownCloud-10
+* https://portal.owncloud.com/apps/files/?dir=/Portal%20Data/All%20Account%20Data/ownCloud%20Enterprise%20Resources%20Data/Server&fileid=5661
+
+EOF
