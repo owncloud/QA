fbafcba13412bb32f41dd40a20ebba94275d9d89
Juergen Weigert <jnweiger@gmail.com>
2024-05-29 16:05:39 +0200

    docker-compose setup, that extends the web service by the ocm (sciencemesh) app.

--- tools/hetzner-deploy/deploy_ocis_traefik.sh
+++ tools/hetzner-deploy/deploy_ocis_traefik.sh
@@ -12,8 +12,12 @@
 
 echo "Estimated setup time (when weather is fine): 6 minutes ..."
 
-compose_subdir=deployments/examples/ocis_traefik
-compose_yml=docker-compose.yml
+compose_dir_orig=/root/ocis/deployments/examples/ocis_traefik
+compose_yml_orig=docker-compose.yml
+# we call the above via a wapper:
+compose_dir=/root/run/ocis_traefik
+compose_yml=local.yml
+
 ocis_bin=/usr/bin/ocis
 
 if [ -z "$OCIS_VERSION" ]; then
@@ -56,9 +60,18 @@ user_speech_url=https://www.einstein-website.de/z_biography/einstein1930.mp3
 
 INIT_SCRIPT <<EOF
 
-echo -e "#! /bin/sh\ncd ~/ocis/$compose_subdir\ndocker-compose -f $compose_yml logs -f --tail=10 --no-color ocis" > /usr/local/bin/show_logs
-echo -e "#! /bin/sh\ncd ~/ocis/$compose_subdir\ndocker-compose -f $compose_yml exec ocis $ocis_bin \"\\\$@\"" > /usr/local/bin/ocis.sh
+echo -e "#! /bin/sh\ncd $compose_dir\ndocker-compose logs -f --tail=10 --no-color ocis" > /usr/local/bin/show_logs
+echo -e "#! /bin/sh\ncd $compose_dir\ndocker-compose exec ocis $ocis_bin \"\\\$@\"" > /usr/local/bin/ocis.sh
 chmod a+x /usr/local/bin/*
+
+# docker-compose v1 or v2?
+## /usr/bin/docker-compose is the legacy v1 python script.
+## docker compose is the supported go implementation of v2. It is missing in 22.04 jammy. We have to add the upstream docker repo
+# FROM: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
+apt update
+apt install -y docker-compose-plugin
 
 cd ~
 ## WORKAROUND: the paths are hardcoded to use docker volumes....
@@ -78,32 +91,90 @@ docker system prune --all --force
 docker volume prune --force
 
 git clone https://github.com/owncloud/ocis.git -b $OCIS_VERSION
-ln -s ocis/$compose_subdir o
-cd o
+ln -s $compose_dir_orig o
+
+mkdir -p run/ocis_traefik{config,data}
+cd $compose_dir
 
 ## .env hacking is always needed. Otherwise we only have localhost endpoints in
 ## curl -k https://$IPADDR/.well-known/openid-configuration | grep https:
 ## and the webpage remains blank. No login possible.
 #
-echo >> .env "# $0 ---- below"
-echo >> .env IPADDR=$IPADDR
-echo >> .env OCIS_VERSION=$OCIS_VERSION
-echo >> .env INSECURE=false
-echo >> .env OCIS_DOCKER_TAG=$OCIS_DOCKER_TAG
-echo >> .env OCIS_DOMAIN=$OCIS_DOMAIN
-echo >> .env INBUCKET_DOMAIN=mail.$BASE_DOMAIN
-echo >> .env TRAEFIK_DOMAIN=traefik.$BASE_DOMAIN
-echo >> .env TRAEFIK_ACME_MAIL=qa@owncloud.com
-echo >> .env TRAEFIK_DASHBOARD=true
-echo >> .env TRAEFIK_BASIC_AUTH_USERS=\$(htpasswd -nb admin $admin_pass)
-echo >> .env ADMIN_PASSWORD=$admin_pass
-echo >> .env DEMO_USERS=true
-echo >> .env OCIS_LOG_LEVEL=debug
+cat <<EOF>>.env
+"# $0 ---- below"
+COMPOSE_FILE=../../ocis/deployments/examples/ocis_traefik/docker-compose.yml:local.yml
+IPADDR=$IPADDR
+OCIS_VERSION=$OCIS_VERSION
+INSECURE=false
+OCIS_DOCKER_TAG=$OCIS_DOCKER_TAG
+OCIS_DOMAIN=$OCIS_DOMAIN
+INBUCKET_DOMAIN=mail.$BASE_DOMAIN
+TRAEFIK_DOMAIN=traefik.$BASE_DOMAIN
+TRAEFIK_ACME_MAIL=qa@owncloud.com
+TRAEFIK_DASHBOARD=true
+TRAEFIK_BASIC_AUTH_USERS=\$(htpasswd -nb admin $admin_pass)
+ADMIN_PASSWORD=$admin_pass
+DEMO_USERS=true
+OCIS_LOG_LEVEL=debug
 ## Seen in $compose_yml
-echo >> .env "## basic auth (not recommended, but needed for eg. WebDav clients that do not support OpenID Connect)"
-echo >> .env "# PROXY_ENABLE_BASIC_AUTH=true"
+## basic auth (not recommended, but needed for eg. WebDav clients that do not support OpenID Connect)
+# PROXY_ENABLE_BASIC_AUTH=true
+EOF
 
-docker-compose -f $compose_yml up -d
+
+## we also add our own local.yml to override some bad defaults, and allow adding apps to the web service.
+cat <<EOF>>local.yml
+---
+services:
+  ocis:
+    environment:
+      XXWEB_UI_CONFIG_FILE: /etc/ocis/web.config-extra.json
+    volumes:
+      - /root/run/ocis_traefik/web.yaml:/etc/ocis/web.yaml
+
+volumes:
+  ocis-config:
+       driver: local
+    driver_opts:
+      o: bind
+      type: none
+      device: /root/run/config
+  ocis-data:
+    driver: local
+    driver_opts:
+      o: bind
+      type: none
+      device: /root/run/data
+EOF
+
+cat <<EOF>>web.config-extra.json
+{
+	"apps": [ "admin-settings", "draw-io", "epub-reader", "external", "files", "ocm", "pdf-viewer", "preview", "search", "text-editor" ]
+}
+EOF
+
+# adding a web.config-extra.json leads to startup errors.
+# adding a web.yaml without setting the WEB_UI_CONFIG_FILE variable works niceley. The name must be web.yaml (my-web-extra.yaml or web.yml do not work!)
+# However, we also must repeat the list of standard apps, we cannot just add one app.
+cat <<EOF>>web.yaml
+common_apps: @common_apps [ "admin-settings", "epub-reader", "external", "files", "pdf-viewer", "preview", "search", "text-editor" ]
+
+web:
+  config:
+    apps:
+      - admin-settings
+      - draw-io
+      - epub-reader
+      - external
+      - files
+      - pdf-viewer
+      - preview
+      - search
+      - text-editor
+      - ocm
+EOF
+
+docker-compose up -d
 
 cat <<EOM > ~/POSTINIT.msg
 
