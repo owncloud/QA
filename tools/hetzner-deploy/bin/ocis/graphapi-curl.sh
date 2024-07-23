f38e30993bb50185a630e58a566309f2db18007e
Juergen Weigert <jnweiger@gmail.com>
2024-07-23 10:16:08 +0200

    fix: support syntax as shown in swagger doc add reference to swagger doc

--- tools/hetzner-deploy/bin/ocis/graphapi-curl.sh
+++ tools/hetzner-deploy/bin/ocis/graphapi-curl.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 # 
+# References:
+#  - https://owncloud.dev/libre-graph-api/#/users/ListUsers
+#
 # We can either use the external port 443 through traffic, e.g. via
 #  wget -d -O - --user=$api_user --password=$api_pass "https://web.$base_fqdn/graph/v1.0/"
 # or an internal port 9200 directly on the container. For the latter, we need to find the correct IPaddress
 #
+# v0.2, 20240723 - jw@owncloud.com - support both /graph/v1.0 and /v1.0 as an optional prefix for the query.
 
 api_user=admin
 api_pass=admin
@@ -17,9 +21,11 @@ if [ "$query" = "" -o "$query" = "-h" -o "$query" = "--help" ]; then
 $0 [graph/v1.0/query]
 
 Well known queries are:
-	groups					# list all groups
-	groups/GROUPNAME			# show only matching group
-	users | jq '.value[].displayName' -r	# show all displayNames of all users.
+	/v1.0/groups					# list all groups
+	/v1.0/groups/GROUPNAME			# show only matching group
+	/v1.0/users | jq '.value[].displayName' -r	# show all displayNames of all users.
+
+See also: https://owncloud.dev/libre-graph-api
 
 environment:
 	OCIS_API_USER		# specify the username for the query. Default: $api_user
@@ -57,8 +63,9 @@ EOF
   exit 1
 fi
 
-# add prefix /graph/v1.0 if missing.
-echo "$query" | grep '^/?graph/v1.0' || query="/graph/v1.0/$query"
+# add prefix /graph/v1.0 if missing in full or partly.
+echo "$query" | grep '^/*v1\.0' && query="/graph/$query"
+echo "$query" | grep '^/*graph//*v1\.0' || query="/graph/v1.0/$query"
 # graph api responds with 404, when it sees dupicate slashes. Remove them.
 query=$(echo "/$query" | sed -e 's@//*@/@g')
 
